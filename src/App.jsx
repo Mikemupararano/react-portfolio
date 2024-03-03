4541c33026792eeaeb7dc5e1cca7db2ab632d34c
Mikemupararano <kudath@yahoo.co.uk>
2024-03-03 15:37:11 +0000

    Installed material icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from './App.module.css';
+import styles from './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './pages/Home';
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, link } from "react-router-dom";
-
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
   return (
@@ -13,6 +13,9 @@ function Navbar() {
         <Link to="/menu"> Menu </Link>
         <Link to="/about"> About</Link>
         <Link to="/contact"> Contact </Link>
+        <button>
+          <MenuIcon />
+        </button>
       </div>
     </div>
   );
