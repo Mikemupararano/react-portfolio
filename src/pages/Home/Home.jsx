ac65d42f16d4ff111382c45960f4d2a589758bfd
Mikemupararano <kudath@yahoo.co.uk>
2024-03-06 22:30:50 +0000

    Fixed footer to bottom.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Home.css'; // Importing the CSS module
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="home">
-      <h1>Mike Thomas</h1>
-      <img src="path/to/your/headhsot.png" alt="Your headshot" />
-      <p>Hi, I am Mike, I am a web developer</p>
-      <p>This is my portfolio site!</p>
+      <div className="headerContainer">
+        <p className="home-description">Hi, I am Mike, I am a web developer!</p>
+        
+        
+      </div>
+      
     </div>
   );
 }
