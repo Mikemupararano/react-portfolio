c7c6674d407f1726689742b2c84143f2ed1cd6cc
Mikemupararano <kudath@yahoo.co.uk>
2024-03-08 18:43:00 +0000

    Coomented out social media icons.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Footer.css'; // Importing the CSS module
 /*import InstagramIcon from '@material-ui/icons/Instagram';*/
-import TwitterIcon from '@material-ui/icons/Twitter';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const Footer = () => {
   return (
@@ -11,9 +11,9 @@ const Footer = () => {
       <div className="socialMedia">
         {/* Render Material-UI icons */}
         {/* <InstagramIcon /> */}
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+        {/* <TwitterIcon /> */}
+        {/* <FacebookIcon /> */}
+        {/* <LinkedInIcon /> */}
       </div>
       <p>&copy; 2024 MT Designs</p>
     </div>
