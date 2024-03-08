e13e01a767ee86b9594c36413136a29ee90a8686
Mikemupararano <kudath@yahoo.co.uk>
2024-03-08 18:39:42 +0000

    Commented out code in Footer.js

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css'; // Importing the CSS module
-import InstagramIcon from '@material-ui/icons/Instagram';
+/*import InstagramIcon from '@material-ui/icons/Instagram';*/
 import TwitterIcon from '@material-ui/icons/Twitter';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
@@ -10,7 +10,7 @@ const Footer = () => {
     <div className="footer">
       <div className="socialMedia">
         {/* Render Material-UI icons */}
-        <InstagramIcon />
+        {/* <InstagramIcon /> */}
         <TwitterIcon />
         <FacebookIcon />
         <LinkedInIcon />
