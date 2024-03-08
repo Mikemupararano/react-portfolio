126c2f1fe85f440708b1140aeee0ec75ee82fdbe
Mikemupararano <kudath@yahoo.co.uk>
2024-03-08 18:32:40 +0000

    Deleted fontawesome in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import "@fontsource/outfit"
-import "@fontsource/roboto"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
