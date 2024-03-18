d4959e8fe24662d6676361cb1f4d4d9335da0b86
Mikemupararano <kudath@yahoo.co.uk>
2024-03-18 21:38:24 +0000

    Modified the contact page.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from "react-router-dom";
 // import mike from './home-images/mike.jpeg'
 import './Home.css'; // Importing the CSS module
+// import DubaiImage from './home-images/dubai.jpeg'
 function HomePage() {
   return (
     <div className="home">
+      {/* <img src={DubaiImage} alt="background image" /> */}
       <div className="headerContainer">
         {/* <img src={mike} alt="Mike's photo." /> */}
         <p className="home-description lead">Hi, I am Mike Thomas! I am a web developer.</p>
